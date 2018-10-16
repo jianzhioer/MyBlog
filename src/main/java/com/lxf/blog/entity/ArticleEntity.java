e69d689435e419805a02e2df201ac75cc4dfe364
sx-9530 <sx-lixf@dtdream.com>
2018-10-16 17:54:12 +0800

    hibernate自动生成数据表配置&命令行输出日志设置

--- src/main/java/com/lxf/blog/entity/ArticleEntity.java
+++ src/main/java/com/lxf/blog/entity/ArticleEntity.java
@@ -1,4 +1,78 @@
 package com.lxf.blog.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "t_article")
 public class ArticleEntity {
+
+    @Id
+    private String articleId;
+    private String title;
+    private String abstracts;
+    private String keywordId;
+    private String labelId;
+    private String createTime;
+    @Column(name = "context",columnDefinition = "longtext")
+    private String context;
+
+    public String getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(String articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAbstracts() {
+        return abstracts;
+    }
+
+    public void setAbstracts(String abstracts) {
+        this.abstracts = abstracts;
+    }
+
+    public String getKeywordId() {
+        return keywordId;
+    }
+
+    public void setKeywordId(String keywordId) {
+        this.keywordId = keywordId;
+    }
+
+    public String getLabelId() {
+        return labelId;
+    }
+
+    public void setLabelId(String labelId) {
+        this.labelId = labelId;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -6,6 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--
 <html>
 <head>
     <title>Title</title>
@@ -14,3 +15,4 @@
 <h1>hello world</h1>
 </body>
 </html>
+--%>
