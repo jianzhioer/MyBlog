657772b0add455fbe2a733a025fe0acdf353b09c
sx-9530 <sx-lixf@dtdream.com>
2018-10-24 10:43:03 +0800

    添加测试service/controller

---
+++ src/main/java/com/lxf/blog/controller/Testestest.java
@@ -0,0 +1,44 @@
+package com.lxf.blog.controller;
+
+import com.lxf.blog.dao.ArticleDao;
+import com.lxf.blog.entity.ArticleEntity;
+import com.lxf.blog.service.TestService;
+import com.lxf.blog.utils.response.ResponseInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+
+/**
+ * @ClassName: Testestest
+ * @Description: TODO
+ * @Author: sx-9530
+ * @CreateDate: 18-10-22
+ * @Version: 1.0
+ */
+@Controller
+@RequestMapping("/test")
+public class Testestest {
+    /*@Resource
+    private ArticleDao articleDao;*/
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping(value="/adsInstanceList",method= RequestMethod.GET)
+    @ResponseBody
+    public ResponseInfo getAdsInstanceList(){
+
+
+        try{
+            String articleEntity = testService.getID("1231646");
+            return ResponseInfo.createSuccessResponse(articleEntity);
+        } catch (Exception e) {
+            e.getStackTrace();
+            return ResponseInfo.createFailedResponse("失败");
+        }
+    }
+}
---
+++ src/main/java/com/lxf/blog/service/TestService.java
@@ -0,0 +1,31 @@
+package com.lxf.blog.service;
+
+import com.lxf.blog.dao.ArticleDao;
+import com.lxf.blog.entity.ArticleEntity;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @ClassName: TestService
+ * @Description: TODO
+ * @Author: sx-9530
+ * @CreateDate: 18-10-22
+ * @Version: 1.0
+ */
+@Service
+public class TestService {
+    @Resource
+    ArticleDao articleDao;
+
+    public String getID(String id){
+        return articleDao.get();
+    }
+
+    public ArticleEntity update(String id){
+        ArticleEntity articleEntity = new ArticleEntity();
+        articleEntity.setTitle("5522222");
+        articleEntity.setArticleId(id);
+        return articleEntity;
+    }
+}
